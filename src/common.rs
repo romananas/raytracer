5819907721c13086f7491ffffaed95b6852bb442
romann <romann.belmer@gmail.com>
2025-02-27 18:33:28 +0100

    minor change

--- src/common.rs
+++ src/common.rs
@@ -6,7 +6,8 @@ pub use std::f64::consts::PI;
 pub use std::f64::INFINITY;
  
 // Utility functions
- 
+
+#[allow(dead_code)] 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
 }
