9cd8e7d66ba34a6ff23b9e1b01d0b97aaf40457c
romann <romann.belmer@gmail.com>
2025-03-11 09:30:27 +0100

    writed documentation

--- src/camera.rs
+++ src/camera.rs
@@ -2,6 +2,44 @@ use crate::common;
 use crate::ray::Ray;
 use crate::vec3::{self, Point3, Vec3};
  
+/// A camera in a 3D space, used for ray tracing.
+///
+/// # Fields
+/// - `origin`: The origin point of the camera.
+/// - `lower_left_corner`: The lower left corner of the viewport.
+/// - `horizontal`: The horizontal vector of the viewport.
+/// - `vertical`: The vertical vector of the viewport.
+/// - `u`: The camera's horizontal axis in world space.
+/// - `v`: The camera's vertical axis in world space.
+/// - `lens_radius`: The radius of the camera's lens.
+///
+/// # Methods
+/// 
+/// ## `new`
+/// Creates a new `Camera` instance.
+/// 
+/// ### Parameters
+/// - `lookfrom`: The point where the camera is located.
+/// - `lookat`: The point where the camera is looking at.
+/// - `vup`: The "up" direction vector.
+/// - `vfov`: Vertical field-of-view in degrees.
+/// - `aspect_ratio`: The aspect ratio of the viewport.
+/// - `aperture`: The aperture size of the camera.
+/// - `focus_dist`: The focus distance of the camera.
+/// 
+/// ### Returns
+/// A new `Camera` instance.
+///
+/// ## `get_ray`
+/// Generates a ray from the camera through the viewport at coordinates (s, t).
+/// 
+/// ### Parameters
+/// - `s`: The horizontal coordinate on the viewport (0.0 to 1.0).
+/// - `t`: The vertical coordinate on the viewport (0.0 to 1.0).
+/// 
+/// ### Returns
+/// A `Ray` instance originating from the camera and passing through the viewport at (s, t).
+#[derive(Debug)]
 pub struct Camera {
     origin: Point3,
     lower_left_corner: Point3,
@@ -11,8 +49,33 @@ pub struct Camera {
     v: Vec3,
     lens_radius: f64,
 }
- 
+
 impl Camera {
+    /// Constructor for a Camera
+    /// 
+    /// # Exemple
+    /// 
+    /// ```
+    ///  use raytracer::{Point3, Vec3};
+    ///  use raytracer::Camera;
+    /// 
+    ///  const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    ///  let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    ///  let lookat = Point3::new(0.0, 0.0, 0.0);
+    ///  let vup = Point3::new(0.0, 1.0, 0.0);
+    ///  let dist_to_focus = 15.0;
+    ///  let aperture = 0.1;
+    ///     
+    ///  let cam = Camera::new(
+    ///      lookfrom,
+    ///      lookat,
+    ///      vup,
+    ///      20.0,
+    ///      ASPECT_RATIO,
+    ///      aperture,
+    ///      dist_to_focus,
+    ///  );
+    /// ```
     pub fn new(
         lookfrom: Point3,
         lookat: Point3,
@@ -49,6 +112,30 @@ impl Camera {
         }
     }
  
+    /// Generate a ray starting from a defined camera and going to the given coordinates.
+    /// 
+    /// # Exemple
+    /// ```
+    /// # use raytracer::{Point3, Vec3};
+    /// use raytracer::Camera;
+    /// 
+    /// # const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    /// # let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    /// # let lookat = Point3::new(0.0, 0.0, 0.0);
+    /// # let vup = Point3::new(0.0, 1.0, 0.0);
+    /// # let dist_to_focus = 15.0;
+    /// # let aperture = 0.1;
+    /// let cam = Camera::new(
+    ///     lookfrom,
+    ///     lookat,
+    ///     vup,
+    ///     20.0,
+    ///     ASPECT_RATIO,
+    ///     aperture,
+    ///     dist_to_focus,
+    /// );
+    /// 
+    /// ```
     pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         let rd = self.lens_radius * vec3::random_in_unit_disk();
         let offset = self.u * rd.x() + self.v * rd.y();
